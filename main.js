11525a0cbedf1d2cf574f1c86e6afd612319d253
jj <maorlevy2010@gmail.com>
2020-03-31 02:27:20 +0300

    paga about the corona

---
+++ main.js
@@ -0,0 +1,13 @@
+var myCanvas = document.createElement('canvas');
+document.appendChild(myCanvas);
+
+var myConfetti = confetti.create(myCanvas, {
+  resize: true,
+  useWorker: true
+});
+myConfetti({
+  particleCount: 100,
+  spread: 160
+  // any other options from the global
+  // confetti function
+});
